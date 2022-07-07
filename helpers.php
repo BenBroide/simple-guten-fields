2335a4dbcd9244323ebc6d859f9924a1acf12f84
Be <=>
2022-07-06 19:06:40 -0700

    fix repeater error

---
+++ helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+function sgf_get_option_dropdown(){
+    return [
+        ['label' => 'Car', 'value' => 1],
+        ['label' => 'Bike', 'value' => 2],
+        ['label' => 'Boat', 'value' => 3],
+    ];
+}
